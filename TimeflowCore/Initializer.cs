5bef8786e668f0c71d7788333cdb42606871bee3
Yaroslav Bondarev <yaroslav.bondarev@advcake.com>
2019-09-16 19:13:25 +0300

    some prototype outlines: create logic, create initializer logic

---
+++ TimeflowCore/Initializer.cs
@@ -0,0 +1,35 @@
+using System;
+using System.IO;
+
+namespace TimeflowCore
+{
+    public class Initializer
+    {
+        public static void CreateTimeLineRepository(string projectDirectoryPath)
+        {
+            CreateTimeLineHiddenDirectory(projectDirectoryPath);
+            CreateTimeLineProjectSnapshotDirectories(projectDirectoryPath);
+        }
+
+        public static bool DoesTimeLineRepositoryExists(string projectDirectoryPath)
+        {
+            var completeTimeLinePath = projectDirectoryPath + "/.timeline";
+            return Directory.Exists(completeTimeLinePath);
+        }
+
+        private static void CreateTimeLineHiddenDirectory(string projectDirectoryPath)
+        {
+            var completeTimeLinePath = projectDirectoryPath + "/.timeline";
+            if (!Directory.Exists(completeTimeLinePath))
+            {
+                Directory.CreateDirectory(".timeline"); 
+            }
+            Console.WriteLine("TimeLine repository has been already created.");
+        }
+
+        private static void CreateTimeLineProjectSnapshotDirectories(string projectDirectoryPath)
+        {
+            var completeTimeLinePath = projectDirectoryPath + "/.timeline";
+        }
+    }
+}
---
+++ TimeflowCore/TimeflowTracker.cs
@@ -0,0 +1,7 @@
+namespace TimeflowCore
+{
+    public class TimeflowTracker
+    {
+        
+    }
+}
