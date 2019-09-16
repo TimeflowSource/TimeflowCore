1297196f8a5d30232a7351bed78dda78e1ec25cc
Yaroslav Bondarev <yaroslav.bondarev@advcake.com>
2019-09-16 19:14:31 +0300

    some prototype outlines: create logic, create initializer logic

--- TimeflowCore/TimeflowEntryPoint.cs
+++ TimeflowCore/TimeflowEntryPoint.cs
@@ -2,11 +2,32 @@
 
 namespace TimeflowCore
 {
-    class TimeflowEntryPoint
+    static class TimeflowEntryPoint
     {
+        private static TimeflowTracker _timeflowTracker;
         static void Main(string[] args)
         {
-            Console.WriteLine("Entry point work");
+            var currentState = true;
+            Console.WriteLine("Enter valid path to directory: ");
+            var requestPath = Console.ReadLine();
+            _timeflowTracker = new TimeflowTracker(requestPath);
+            while (true)
+            {
+                var command = Console.ReadLine();
+                if (command == "init")
+                    _timeflowTracker.Init();
+                if (command == "exit")
+                    _timeflowTracker.Start();
+                else if (command == "start")
+                    _timeflowTracker.Stop();
+                else if (command == "stop")
+                    currentState = false;
+                if (!currentState)
+                {
+                    break;
+                }
+            }
         }
     }
+
 }
--- TimeflowCore/TimeflowTracker.cs
+++ TimeflowCore/TimeflowTracker.cs
@@ -1,7 +1,32 @@
+using System;
+
 namespace TimeflowCore
 {
     public class TimeflowTracker
-    {
+    {      
+        public string ProjectDirectoryPath { get; set; }
+
+        public TimeflowTracker(string projectPath)
+        {
+            this.ProjectDirectoryPath = projectPath;
+        }
         
+        public void Init()
+        {
+            try
+            {
+            }
+            catch (Exception e)
+            {
+            }
+        }
+
+        public void Start()
+        {
+        }
+
+        public void Stop()
+        {
+        }
     }
 }
