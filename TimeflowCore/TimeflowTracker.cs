3805bb881d67188e42176c495875f1bdd12bb2b9
Yaroslav Bondarev <yaroslav.bondarev@advcake.com>
2019-09-18 20:41:59 +0300

    add files creation

--- TimeflowCore/TimeflowTracker.cs
+++ TimeflowCore/TimeflowTracker.cs
@@ -1,21 +1,29 @@
 using System;
 using System.IO;
+using System.Security.Cryptography;
 
 namespace TimeflowCore
 {
     public class TimeflowTracker : FileSystemWatcher
     {
         public string ProjectDirectoryPath { get; set; }
+        public string TimeflowPath { get; set; }
         public FileSystemWatcher Watcher { get; set; }
+
+        private static int _renamedIndex = 0;
+        private static int _changedIndex = 0;
+        private static int _createdIndex = 0;
+        private static int _deletedIndex = 0;
 
         public TimeflowTracker(string projectPath)
         {
             this.ProjectDirectoryPath = projectPath;
+            this.TimeflowPath = projectPath + "/.timeflow";
             this.Watcher = new FileSystemWatcher(projectPath);
             try
             {
                 Initializer.CreateTimeflowRepository(this.ProjectDirectoryPath);
-                this.Watcher.NotifyFilter =  NotifyFilters.LastWrite
+                this.Watcher.NotifyFilter = NotifyFilters.LastWrite
                                             | NotifyFilters.FileName
                                             | NotifyFilters.DirectoryName;
                 this.Watcher.IncludeSubdirectories = true;
@@ -35,21 +43,49 @@ namespace TimeflowCore
         private void _OnRenamed(object sender, RenamedEventArgs e)
         {
             Console.WriteLine($"File: {e.FullPath} {e.ChangeType}");
+            try
+            {
+                File.Copy(e.FullPath, TimeflowPath + $"/{e.Name}-{e.ChangeType}.txt");
+            }
+            catch (Exception exception)
+            {
+            }
         }
 
         private void _OnDeleted(object sender, FileSystemEventArgs e)
         {
             Console.WriteLine($"File: {e.FullPath} {e.ChangeType}");
+            try
+            {
+                File.Copy(e.FullPath, TimeflowPath + $"/{e.Name}-{e.ChangeType}.txt");
+            }
+            catch (Exception exception)
+            {
+            }
         }
 
         private void _OnCreated(object sender, FileSystemEventArgs e)
         {
             Console.WriteLine($"File: {e.FullPath} {e.ChangeType}");
+            try
+            {
+                File.Copy(e.FullPath, TimeflowPath + $"/{e.Name}-{e.ChangeType}.txt");
+            }
+            catch (Exception exception)
+            {
+            }
         }
 
         private void _OnChanged(object sender, FileSystemEventArgs e)
         {
             Console.WriteLine($"File: {e.FullPath} {e.ChangeType}");
+            try
+            {
+                File.Copy(e.FullPath, TimeflowPath + $"/{e.Name}-{e.ChangeType}.txt");
+            }
+            catch (Exception exception)
+            {
+            }
         }
     }
 }
